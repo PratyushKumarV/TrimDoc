d7a18a02aee1caa1d229a32b814451aaba36e778
PratyushKumarV <pratyushkumarv@gmail.com>
2024-12-01 15:15:06 +0530

    Added file upload functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+const compressbtn=document.querySelector("#compress");
+const compressInput=document.querySelector("#compress-input");
+const splitbtn=document.querySelector("#split");
+const splitInput=document.querySelector("#split-input");
+
+compressbtn.addEventListener("click", ()=>{
+    compressInput.click() //simulates a button click for the input element when the compress button is clicked
+})
+
+splitbtn.addEventListener("click", ()=>{
+    splitInput.click()
+})
